2278ecda6984681e7810b03fd261526297da7f89
KrystynaGrzesiak <krygrz11@gmail.com>
2020-07-09 12:57:12 +0200

    updates package

--- R/list_mean.R
+++ R/list_mean.R
@@ -1,5 +1,5 @@
 #' @title Mean of list
-#' @description  Compute mean of vectors
+#' @description  Computes bardzo ładnie mean of vectors
 #' @param x list of vectors
 #' @export
 #' @details This function uses \code{\link[base]{lapply}} and  \code{\link[base]{mean}} .
